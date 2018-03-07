6125976d5ea353acf79f21f97698c42ca5533810
Kateryna Kononenko <k.konone@wustl.edu>
2018-03-06 18:00:15 -0600

    demo

---
+++ Coffee_Ordering/OrderItem.swift
@@ -0,0 +1,9 @@
+//
+//  OrderItem.swift
+//  Coffee_Ordering
+//
+//  Created by Kateryna Kononenko on 3/3/18.
+//  Copyright © 2018 Kateryna Kononenko. All rights reserved.
+//
+
+import Foundation
